9fd66a810906fb53bcb59ce7ec59c64126e57845
itict10 <itict10@gmail.com>
2023-04-05 00:05:26 +0900

    add linter and etc

--- javascript-playwright/main.ts
+++ javascript-playwright/main.ts
@@ -1,30 +1,27 @@
-import { chromium } from '@playwright/test';
+import { chromium } from '@playwright/test'
 
-(async () => {
+void (async () => {
   const browserOptions = {
-    headless: true,
-    // slowMo: 100,
+    headless: true
     // proxy: {
     //   server: 'http://proxy.example.com:8080',
     //   username: 'username',
     //   password: 'password',
     // }
-  };
-  const browser = await chromium.launch(browserOptions);
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  }
 
+  const browser = await chromium.launch(browserOptions)
+  const context = await browser.newContext()
+  const page = await context.newPage()
 
-  await page.goto('https://www.nikkei.com/');
+  await page.goto('https://www.nikkei.com/')
 
-
-
-  const detail_urls = await page.$$eval(
+  const detailUrls = await page.$$eval(
     'article h2 a[href*="/article/"]',
-    (links: HTMLLinkElement[])  => links.map(a => a.href)
-  );
+    (links: HTMLLinkElement[]) => links.map(a => a.href)
+  )
 
-  console.log(detail_urls);
+  console.log(detailUrls)
 
-  await browser.close();
-})();
+  await browser.close()
+})()
